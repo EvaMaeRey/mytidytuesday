ac28038c1626210ecd0a3fa612ec93ca011e5bf5
EvaMaeRey <evangeline.mae@gmail.com>
2024-07-12 12:08:25 -0600

    using ggproto to make Stats and use directly in geom_ functions

--- embedded_flipbooks/helloworld.R
+++ embedded_flipbooks/helloworld.R
@@ -2,4 +2,13 @@ library(ggplot2)
 cars |>
   ggplot() + 
   aes(x = speed, y = dist) +
-  geom_point(size = 6)
+  geom_point() + 
+  aes(size = I(8)) + 
+  aes(alpha = I(.7)) + 
+  aes(color = I("darkolivegreen")) + 
+  aes(shape = I(21)) + 
+  aes(fill = I("whitesmoke")) + 
+  aes(stroke = I(1.5)) + 
+  geom_density2d() + 
+  aes(linewidth = I(1)) + 
+  aes(shape = I(22))
