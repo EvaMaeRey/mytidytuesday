ba85d392cc5d97ec8b36f511f13a3f91d56b025f
EvaMaeRey <evangeline.mae@gmail.com>
2024-10-07 15:00:47 -0600

    bricks layer expreiment

--- embedded_flipbooks/gtsum3.R
+++
@@ -1,9 +0,0 @@
-names(gtstable)
-
-str(gtstable$table_body) ->
-  shhh; str(gtstable$table_styling) ->
-  shhh; str(gtstable$call_list) ->
-  shhh; str(gtstable$cards) ->
-  shhh; str(gtstable$inputs)
-
-
