1f46191ae9188ab9d4e63d12b8ec137ff97404b4
EvaMaeRey <evangeline.mae@gmail.com>
2025-11-17 11:58:06 -0700

    stat split but with alpha keeping track of what step you are on

--- embedded_flipbooks/beauty.R
+++
@@ -1,8 +0,0 @@
-ggpipedpairs(tips) |>
-  set_columns(c("total_bill", "time", "tip")) |> 
-  set_lower() |> # brings in ggpairs defaults  
-  set_mapping(aes(color = time)) |> 
-  set_mapping(aes(color = time, alpha = I(.7))) |> 
-  set_upper() |>
-  set_title("A ggpairs plot") |>
-  set_columns(c("total_bill", "tip", "size")) 
