db75f4dc4f91eba526ae755837994521cc492c60
EvaMaeRey <evangeline.mae@gmail.com>
2026-07-31 22:35:00 -0600

    more curry

--- 2026_02_20_flipbooks/cliprcode.R
+++
@@ -1,8 +0,0 @@
-gapminder::gapminder |> 
-  filter(country %in% c("France") ) |> 
-  ggplot() + 
-  aes(x = year,
-      y = lifeExp) + 
-  geom_line() + 
-  aes(y2 = gdpPercap) +
-  geom_line_sec_axis()
